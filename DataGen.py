1444c56a08f4d16814d4c8fbe8cd1c5189915b1d
nvtinh368 <nvtinh368@gmail.com>
2022-06-30 16:31:56 +0700

    werwer

--- DataGen.py
+++ DataGen.py
@@ -1,9 +1,9 @@
 import random
 import string
 
-startPort = 22610
-endPort = 22619 + 1
-ipv4 = '143.198.67.182'
+startPort = 23500
+endPort = 24300 + 1
+ipv4 = '103.214.9.13'
 
 
 def GeneatorPass():
@@ -14,21 +14,24 @@ def GeneratorIPv6Part(n):
     return ''.join(random.choices(array, k=n))
 
 def GeneratorIPv6():
-    ipv6 = f'2001:470:66:1fa:{GeneratorIPv6Part(4)}:{GeneratorIPv6Part(4)}:{GeneratorIPv6Part(4)}:{GeneratorIPv6Part(4)}'
+    ipv6 = f'2405:19c0:1{GeneratorIPv6Part(3)}:{GeneratorIPv6Part(4)}:{GeneratorIPv6Part(4)}:{GeneratorIPv6Part(4)}:{GeneratorIPv6Part(4)}:{GeneratorIPv6Part(4)}'
     return ipv6
-
+firewall = open('data\open.txt','w')
 proxyFile = open('data\proxy.txt','w')
 dataFile = open('data\data.txt','w')
 dataFile.write(f'cat <<EOF >data.txt \n')
+dataFile.write(f'EOF\n')
+dataFile.write(f'\n')
 for i in range(startPort,endPort):
     password = GeneatorPass()
     ipv6 = GeneratorIPv6()
     line = f'user{i}/{password}/{ipv4}/{i}/{ipv6}'
     proxy = f'{ipv4}:{i}:user{i}:{password}'
     proxyFile.write(f'{proxy}\n')
+    firewall.write(f'firewall-cmd --zone=public --add-port={i}/tcp --permanent\n')
     dataFile.write(f'{line}\n')
     print(line)
-dataFile.write(f'EOF\n')
-dataFile.write(f'\n')
+firewall.write('firewall-cmd --reload\n')
+firewall.close()
 proxyFile.close()
 dataFile.close()
